5bd541069d46222455fbcab2aa653b0e845f1605
RLucasLeo <lucas3leo94@gmail.com>
2024-08-30 23:58:32 -0300

    lista de empleados creada

---
+++ src/empleados/ListadoEmpleados.jsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+export default function ListadoEmpleados() {
+
+    const urlBase= "http://localhost:8080/rh-app/empleados";
+
+    const [empleados, setEmpleados] = useState([]);
+
+    useEffect(()=>{
+        cargarEmpleados();
+    }, []);
+
+    const cargarEmpleados = async ()=>{
+        const resultado = await axios.get(urlBase);
+        console.log("Resultado de cargar empleados" + resultado.data);
+        setEmpleados(resultado.data);
+    }
+
+
+
+  return (
+    <div className='container'>
+        <div className='container text-center' style={{margin: "30px"}}>
+            <h3>Sistema de Recursos Humanos</h3>
+        </div>
+        
+        <table className="table table-striped table-hover align-middle">
+            <thead className='table-dark'>
+                <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Empleado</th>
+                <th scope="col">Departamento</th>
+                <th scope="col">Sueldo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+  )
+}
