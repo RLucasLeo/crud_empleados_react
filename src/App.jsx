05ade37ed4576cb4b7ed6edee0fe644e469e27d5
RLucasLeo <lucas3leo94@gmail.com>
2024-08-31 17:42:12 -0300

    ruta /edit empl agregada

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ListadoEmpleados from './empleados/ListadoEmpleados';
 import Navegacion from './plantillas/Navegacion';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AgregarEmpleado from './plantillas/AgregarEmpleado';
+import EditarEmpleado from './empleados/EditarEmpleado';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<ListadoEmpleados/>}/>
         <Route exact path='/agregar' element={<AgregarEmpleado/>}/>
+        <Route exact path='/editar/:id' element={<EditarEmpleado/>}/>
       </Routes>
     </BrowserRouter>
       </div>);
