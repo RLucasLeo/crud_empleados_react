9080b4fbe6fd4678bbc115ced4e06766e537214d
RLucasLeo <lucas3leo94@gmail.com>
2024-08-31 11:32:53 -0300

    navbar agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ListadoEmpleados from './empleados/ListadoEmpleados';
+import Navegacion from './plantillas/Navegacion';
 
 function App() {
 
   return (
-    <ListadoEmpleados></ListadoEmpleados>
-  );
+    <div className='container'>
+    <Navegacion/>
+    <ListadoEmpleados/>
+      </div>);
 }
 
 export default App
