3bad25fd1375225c139bfab259fc1ef75f6854e5
RLucasLeo <lucas3leo94@gmail.com>
2024-09-04 11:06:54 -0300

    rutas autenticacion agregadas

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Navegacion from './plantillas/Navegacion';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AgregarEmpleado from './plantillas/AgregarEmpleado';
 import EditarEmpleado from './empleados/EditarEmpleado';
+import Login from './autenticacion/Login';
+import Registro from './autenticacion/Registro';
 
 function App() {
 
   return (
     <div className='container'>
     <BrowserRouter>
-      <Navegacion/>
+        {location.pathname !== '/' || location.pathname !== '/registro' && <Navegacion/>}
       <Routes>
-        <Route exact path='/' element={<ListadoEmpleados/>}/>
+        <Route exact path='/' element={<Login/>}/>
+        <Route exact path='/registro' element={<Registro/>}/>
+        <Route exact path='/empleados' element={<ListadoEmpleados/>}/>
         <Route exact path='/agregar' element={<AgregarEmpleado/>}/>
         <Route exact path='/editar/:id' element={<EditarEmpleado/>}/>
       </Routes>
